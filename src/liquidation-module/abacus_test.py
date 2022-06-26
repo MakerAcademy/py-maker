878399af244a5f679338bfe52573154c63bcca1a
GodlyDog <damonburdell@gmail.com>
2022-06-25 23:19:00 -0500

    abacus testing

---
+++ src/liquidation-module/abacus_test.py
@@ -0,0 +1,30 @@
+import math
+
+from abacus import LinearDecrease, ExponentialDecrease, StairstepExponentialDecrease
+import unittest
+
+
+class AbacusTest(unittest.TestCase):
+    def test_linear_decrease(self):
+        abacus = LinearDecrease(20.0)
+        self.assertTrue(abacus.price(100.0, 10.0) == 50.0)
+        self.assertTrue(abacus.price(100.0, 20.0) == 0.0)
+        self.assertTrue(abacus.price(100.0, 30.0) == 0.0)
+
+    def test_exponential_decrease(self):
+        abacus = ExponentialDecrease(0.1)
+        self.assertTrue(abacus.price(100.0, 1.0) == 10.0)
+        self.assertTrue(math.isclose(abacus.price(100.0, 2.0), 1.0))
+        self.assertTrue(math.isclose(abacus.price(100.0, 3.0), 0.1))
+
+    def test_stairstep_exponential_decrease(self):
+        abacus = StairstepExponentialDecrease(1.0, 0.1)
+        self.assertTrue(abacus.price(100.0, 1.0) == 10.0)
+        self.assertTrue(math.isclose(abacus.price(100.0, 2.0), 1.0))
+        self.assertTrue(math.isclose(abacus.price(100.0, 3.0), 0.1))
+
+
+if __name__ == '__main__':
+    print("Testing...")
+    unittest.main()
+    print("All tests passed.")
